04e129772192c45cf1e1ac7524538dbd8d5b68d9
Franco Ciprian <franco_ciprian@hotmail.com>
2022-11-28 19:00:17 -0300

    Changing(Sidebar)

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { IconDashboard, IconProducts, IconNewUser } from "../assets/Icons";
+import { IconDashboard, IconProducts, IconNewUser, IconNewProduct } from "../assets/Icons";
 
 const Sidebar = () => {
   const [open, setOpen] = useState(true);
@@ -11,21 +11,29 @@ const Sidebar = () => {
       link: "/"
     },
     {
-      title: "Products",
-      src: IconProducts,
-      link: "/products"
-    },
-    {
       title: "New User",
       src: IconNewUser,
-      link: "/user/new",
+      link: "/user/new"
+    },
+    {
+      title: "Products",
+      src: IconNewProduct,
+      // link: '/product/new',
       gap: true
+    },
+    {
+      title: "New Product",
+      src: IconProducts,
+      // link: "/products",
     },
   ];
 
   return (
     <div className="min-h-screen bg-white">
-      <div className={` ${open ? "w-72" : "w-20 "} bg-dark-purple h-screen p-5 relative duration-500`} >
+      <div 
+        // className={` ${open ? "w-72" : "w-20 "} bg-dark-purple h-screen p-5 relative duration-500`} 
+        className=" w-60 bg-dark-purple h-screen p-5 relative duration-500"
+        >
         <div className="flex gap-x-4 items-center">
           <h1 className='text-white origin-left font-medium text-xl duration-200'>CRM<span className={!open && "hidden"}>Admin</span></h1>
         </div>
@@ -45,7 +53,7 @@ const Sidebar = () => {
           ))}
         </ul>
 
-        <div className="absolute bottom-12">
+        {/* <div className="absolute bottom-12">
           <button 
             onClick={() => setOpen(!open)}  
             className='flex p-2 cursor-pointer text-gray-300 text-sm items-center gap-x-4'
@@ -60,7 +68,7 @@ const Sidebar = () => {
               </svg>
             }
           </button>
-        </div>
+        </div> */}
       </div>
     </div>
   );
